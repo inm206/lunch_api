f2c95d4347a48bfeb703f310a7b0a2b2b9cbbcfb
Isaac Mbira <inm206@nyu.edu>
2020-03-13 21:59:20 +1100

    Added basic unit and integration tests

--- src/test/java/com/rezdy/lunchapi/LunchApiApplicationTests.java
+++ src/test/java/com/rezdy/lunchapi/LunchApiApplicationTests.java
@@ -21,10 +21,20 @@ class LunchApiApplicationTests {
 	private MockMvc mvc;
 
 	@Test
-	public void getHello() throws Exception {
+	public void getIndex() throws Exception {
 		mvc.perform(MockMvcRequestBuilders.get("/").accept(MediaType.APPLICATION_JSON))
 				.andExpect(status().isOk())
-				.andExpect(content().string(equalTo("Got spring functional")));
+				.andExpect(content().string(equalTo("To get lunch recipes, make a GET request" +
+						" to /lunch instead")));
+	}
+
+	@Test
+	public void getRecipes() throws Exception {
+		mvc.perform(MockMvcRequestBuilders.get("/lunch").accept(MediaType.APPLICATION_JSON))
+				.andExpect(status().isOk())
+				.andExpect(content().string(equalTo("[{\"title\":\"Ham and Cheese Toastie\"," +
+						"\"ingredients\":[\"Ham\",\"Cheese\",\"Bread\",\"Butter\"]},{\"title\":\"Hotdog\"," +
+						"\"ingredients\":[\"Hotdog Bun\",\"Sausage\",\"Ketchup\",\"Mustard\"]}]")));
 	}
 
 }
