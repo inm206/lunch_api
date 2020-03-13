f7c1d6eb3a84620f911c9cf35b9acd7743530d38
Isaac Mbira <inm206@nyu.edu>
2020-03-13 20:40:32 +1100

    Completed acceptance criteria for recipe fitlering, now also sorts based on items passed best before date but still within use by date

--- src/main/java/com/rezdy/lunch_api/MainController.java
+++ src/main/java/com/rezdy/lunch_api/MainController.java
@@ -10,6 +10,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.client.RestTemplate;
 
+import java.util.Collections;
+import java.util.Comparator;
 import java.util.Date;
 import java.util.List;
 import java.util.function.Predicate;
@@ -21,6 +23,9 @@ public class MainController {
     private List<Recipe> possibleRecipes;
     private List<Ingredient> ingredients;
     private List<String> safeIngredients;
+    private List<String> notBestIngredients;
+
+    private List<Recipe> bestBeforeFlag;
 
     private static final Logger log = LoggerFactory.getLogger(LunchApiApplication.class);
 
@@ -42,11 +47,19 @@ public class MainController {
             Date todayDate = new Date();
 //            DateTimeComparator dateTimeComparator = DateTimeComparator.getDateOnlyInstance();
             Predicate<Ingredient> byUseBy = ingredient -> ingredient.getUseBy().compareTo(todayDate) == 1;
+            Predicate<Ingredient> byBestBefore = ingredient -> ingredient.getBestBefore().compareTo(todayDate) == -1;
             this.safeIngredients = ingredients.stream().filter(byUseBy).map(p-> p.getTitle()).collect(Collectors.toList());
-            log.info(safeIngredients.toString());
+            this.notBestIngredients = ingredients.stream().filter(byUseBy).filter(byBestBefore).map(p-> p.getTitle()).collect(Collectors.toList());
+//            log.info(safeIngredients.toString());
+            log.info(notBestIngredients.toString());
 
             Predicate<Recipe> byAvailable = recipe -> safeIngredients.containsAll(recipe.getIngredients());
+            Predicate<Recipe> byBestBeforeFlag = recipe -> !Collections.disjoint(recipe.getIngredients(), notBestIngredients);
             this.possibleRecipes = recipes.stream().filter(byAvailable).collect(Collectors.toList());
+            log.info(possibleRecipes.toString());
+
+            this.bestBeforeFlag = this.possibleRecipes.stream().filter(byBestBeforeFlag).collect(Collectors.toList());
+            Collections.sort(this.possibleRecipes, Comparator.comparing(recipe -> bestBeforeFlag.contains(recipe)));
             log.info(possibleRecipes.toString());
         };
     }
